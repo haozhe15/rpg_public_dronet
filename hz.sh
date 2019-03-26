925357c2952eae412c455445ca1b9b6a4c662813
Haozhe <wanghaozhe15@gmail.com>
2019-03-25 23:41:20 -0400

    add new exp

--- hz.sh
+++ hz.sh
@@ -1,13 +1,20 @@
+# steer angle
+python cnn.py --experiment_rootdir='./model/exp_01_1500' --train_dir='datasets/steering_dataset/training' --val_dir='datasets/steering_dataset/validation' --batch_size=16 --epochs=1 --log_rate=25 
 python evaluation.py --experiment_rootdir='./model' --weights_fname='model_weights.h5' --test_dir='datasets/steering_dataset/testing' 
-python cnn.py --experiment_rootdir='./model/test_1' --train_dir='datasets/steering_dataset/training' --val_dir='datasets/steering_dataset/validation' --batch_size=16 --epochs=1 --log_rate=25 
 
-ython evaluation.py --experiment_rootdir='./model' --weights_fname='model_weights.h5' --test_dir='datasets/collision_dataset/testing' 
-python cnn.py --experiment_rootdir='./model/test_1' --train_dir='datasets/collision_dataset/training' --val_dir='datasets/collision_dataset/validation' --batch_size=16 --epochs=1 --log_rate=25 
+# collision probability
+python cnn.py --experiment_rootdir='./model/exp_01_1500' --train_dir='datasets/collision_dataset/training' --val_dir='datasets/collision_dataset/validation' --batch_size=16 --epochs=1 --log_rate=25 
+python evaluation.py --experiment_rootdir='./model' --weights_fname='model_weights.h5' --test_dir='datasets/collision_dataset/testing' 
 
 sudo find ~/Git -name ".DS_Store" -depth -exec rm {} \;
 
+# plot
 python plot_loss.py --experiment_rootdir model/s_test_2
-
 python plot_results.py --experiment_rootdir model/s_test_2
 
 python data_preprocessing/time_stamp_matching.py
+
+# Set up git 
+git config --global user.name haozhe15
+git config --global user.email wanghaozhe15@gmail.com
+git config --global credential.helper cache
